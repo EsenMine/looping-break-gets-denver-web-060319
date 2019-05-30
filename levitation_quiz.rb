30bf4e460d0a50bc397387067066b1df0627b471
Miné E <mine_esen@yahoo.ca>
2019-05-30 00:06:40 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
-	#your code here
+	puts "What is teh spell that enacts levitation?"
+  loop do
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+    puts "You passed the quiz!"
+  end
 end
 
 
